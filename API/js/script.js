89552b1b3bdfcef2c3611e3a8ac5269b04a5d50d
ILauro <48037091+ILauro@users.noreply.github.com>
2020-10-02 22:04:10 -0300

    Update script.js

--- API/js/script.js
+++ API/js/script.js
@@ -1,1 +1,14 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    const url = "";
+    fetch(url)
+    .then(response => response.json())
+    .then(info => {
+        
+    })
+    
+});
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
 
