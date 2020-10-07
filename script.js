859cb2304f4434cd59dbe4416c67638e77f77f40
ILauro <48037091+ILauro@users.noreply.github.com>
2020-10-06 22:40:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
- fetch("https://covid19-brazil-api.now.sh/api/report/v1/brazil", {
+ fetch("https://covid19-brazil-api.now.sh/api/report/v1/germany", {
   "method": "GET"
 })
 .then(async response => await console.log(response.json()))
