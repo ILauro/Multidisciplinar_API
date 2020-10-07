a1f9e584883be340ce585b1d581972a983a402d9
ILauro <48037091+ILauro@users.noreply.github.com>
2020-10-06 22:26:47 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+
+ fetch("https://covid19-brazil-api.now.sh/api/report/v1", {
+  "method": "GET"
+})
+.then(response => console.log(response))
+.catch(err => console.error(err));
+
+
+});
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
