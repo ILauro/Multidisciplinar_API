08f253339362a3375838742e24eb5ede01121f5e
ILauro <48037091+ILauro@users.noreply.github.com>
2020-10-06 21:26:58 -0300

    Update script.js

--- API/js/script.js
+++ API/js/script.js
@@ -1,11 +1,20 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    const url = "";
-    fetch(url)
-    .then(response => response.json())
-    .then(info => {
-        
-    })
-    
+const url = 'http://www.portaltransparencia.gov.br/api-de-dados/cartoes?mesExtratoInicio=11%2F2019&mesExtratoFim=10%2F2020&pagina=1'
+
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET","https://cors-anywhere.herokuapp.com/%22+url);
+xhr.setRequestHeader("chave-api-dados", "841ae6c076331d1535becdcce757fd2c");
+xhr.setRequestHeader("Accept", "/");
+xhr.send(null);
+
+xhr.onreadystatechange = function() {
+    if(xhr.readyState === 4){
+        console.log(JSON.parse(xhr.responseText)
+        );
+    }
+}
+
 });
 
 function capitalizeFirstLetter(string) {
