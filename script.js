7b01acc42a2f68a7fb8f284f249491aed7ac9d4a
ILauro <48037091+ILauro@users.noreply.github.com>
2020-10-06 22:35:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
  fetch("https://covid19-brazil-api.now.sh/api/report/v1", {
   "method": "GET"
 })
-.then(response => console.log(response.json()))
+.then(response => console.log(response.clone().json()))
 .catch(err => console.error(err));
 
 
