a91b354d7e34f6fd995344615fc3ec46fc5f518b
ILauro <lauroseibert@gmail.com>
2020-10-08 14:51:48 -0300

    dasdasd

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-
-var data = new XMLHttpRequest();
-data.open("GET","http://www.transparencia.gov.br/api-de-dados/bolsa-familia-por-municipio?mesAno=202006&codigoIbge=%094312401&pagina=1");
-data.send(null);
-
-data.onreadystatechange = function() {
-    if(data.readyState === 4){
-        console.log(JSON.parse(data.responseText)
-        );
-    }
-}
-
-
-});
+    const url = 'http://www.portaltransparencia.gov.br/api-de-dados/auxilio-emergencial-por-municipio?mesAno=07%2F2020&codigoIbge=4308102&pagina=1 ';
+    
+    var xhr = new XMLHttpRequest();
+    
+    xhr.open("GET","https://cors-anywhere.herokuapp.com/%22+url);
+    xhr.setRequestHeader("chave-api-dados", "3ba770070f17c5875f2755bdfd2cff7a");
+    xhr.setRequestHeader("Accept", "/");
+    xhr.send(null);
+    
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState === 4){
+           console.log(JSON.parse(xhr.responseText));
+    
+            }
+        }
+    
+    });
 
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
