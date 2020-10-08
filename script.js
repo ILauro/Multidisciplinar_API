c9f3e87c88f9775c198c53451ca4f6b37fb15f81
ILauro <lauroseibert@gmail.com>
2020-10-08 16:38:02 -0300

    Select e js parte

--- script.js
+++ script.js
@@ -1,22 +1,40 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    const url = 'http://www.portaltransparencia.gov.br/api-de-dados/auxilio-emergencial-por-municipio?mesAno=07%2F2020&codigoIbge=4308102&pagina=1 ';
+    const url = 'http://www.transparencia.gov.br/api-de-dados/bolsa-familia-por-municipio?mesAno=202006&codigoIbge=4312401&pagina=1';
     
     var xhr = new XMLHttpRequest();
     
-    xhr.open("GET","https://cors-anywhere.herokuapp.com/%22+url);
+    xhr.open("GET","https://cors-anywhere.herokuapp.com/"+url);
     xhr.setRequestHeader("chave-api-dados", "3ba770070f17c5875f2755bdfd2cff7a");
-    xhr.setRequestHeader("Accept", "/");
+    xhr.setRequestHeader("Accept", '*/*');
     xhr.send(null);
     
     xhr.onreadystatechange = function() {
         if(xhr.readyState === 4){
            console.log(JSON.parse(xhr.responseText));
-    
             }
         }
     
     });
 
-function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
-}
+
+    
+document.getElementById("bt").addEventListener("click", function() {
+    const url = 'http://www.transparencia.gov.br/api-de-dados/bolsa-familia-por-municipio?mesAno=202006&codigoIbge=4312401&pagina=1';
+    
+    var xhr = new XMLHttpRequest();
+    
+    xhr.open("GET","https://cors-anywhere.herokuapp.com/"+url);
+    xhr.setRequestHeader("chave-api-dados", "3ba770070f17c5875f2755bdfd2cff7a");
+    xhr.setRequestHeader("Accept", '*/*');
+    xhr.send(null);
+    
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState === 4){
+           console.log(JSON.parse(xhr.responseText));
+            }
+        }
+});
+
+// function capitalizeFirstLetter(string) {
+//     return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+// }
