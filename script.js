228434959d9866634475c7d18efe3e9c706171bc
ILauro <lauroseibert@gmail.com>
2020-10-08 19:55:01 -0300

    Função, select, botão

--- script.js
+++ script.js
@@ -18,8 +18,29 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 
     
-document.getElementById("bt").addEventListener("click", function() {
-    const url = 'http://www.transparencia.gov.br/api-de-dados/bolsa-familia-por-municipio?mesAno=202006&codigoIbge=4312401&pagina=1';
+function res() {
+    const url = 'http://www.transparencia.gov.br/api-de-dados/bolsa-familia-por-municipio?mesAno=202006&codigoIbge=' + document.getElementById("select").value + '&pagina=1';
+    
+    var xhr = new XMLHttpRequest();
+    
+    xhr.open("GET","https://cors-anywhere.herokuapp.com/"+url);
+    xhr.setRequestHeader("chave-api-dados", "3ba770070f17c5875f2755bdfd2cff7a");
+    xhr.setRequestHeader("Accept", '*/*');
+    xhr.send(null);
+    
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState === 4){
+            document.getElementById('bt').innerHTML = JSON.parse(xhr.responseText)['0']['municipio'].nomeIBGE;
+            }
+        }
+};
+
+
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+function date(int, string) {
+    const url = 'http://www.transparencia.gov.br/api-de-dados/bolsa-familia-por-municipio?mesAno=2020' + int +'&codigoIbge=' + string + '&pagina=1';
     
     var xhr = new XMLHttpRequest();
     
@@ -33,7 +54,8 @@ document.getElementById("bt").addEventListener("click", function() {
            console.log(JSON.parse(xhr.responseText));
             }
         }
-});
+
+}
 
 // function capitalizeFirstLetter(string) {
 //     return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
